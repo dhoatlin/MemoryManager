11fe6629ade9c56ce066dec374c33dd7b71eeb5f
Dave Hoatlin <dhoatlin@gmail.com>
2011-04-11 22:08:41 -0400

    converted layout to use grid

--- mm.py
+++ mm.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python
 
-import Tkinter, tkFileDialog
 from Tkinter import *
+from tkFileDialog import *
 
+#browse function for loading a file
 def browse():
-  file = tkFileDialog.askopenfile(parent=root, mode='rb',title='Choose a file')
+  file = askopenfile(parent=root, mode='rb',title='Choose a file')
   if file != None:
-    data = file.read()
+    #enable writing to textbox
+    inputbox.config(state=NORMAL)
+    
+    #write file to textbox
+    for line in file:
+      inputbox.insert(END, line)
     file.close()
-    print "I got %d bytes from this file." % len(data)
-    loaded = True
-    browseButton.config(state=DISABLED)
+    
+    #disbale writing to textbox
+    inputbox.config(state=DISABLED)
 
-root = Tkinter.Tk()
-frame = Frame(root, height=500, width=500)
-frame.pack_propagate(0)
-frame.pack()
+#create the main window
+root = Tk()
+root.title('Memory Manager - Dave Hoatlin')
+
 
 #setup the menubar
 menubar = Menu(root)
@@ -26,7 +32,19 @@ fileMenu.add_command(label='Exit', command=root.quit)
 menubar.add_cascade(label='File', menu=fileMenu)
 root.config(menu=menubar)
 
-browseButton = Button(frame, text='load', command=browse)
-browseButton.pack()
+#creating a textbox
+inputFrame=Frame(root)
+inputbox = Text(inputFrame, height=10, width=30, state=DISABLED)
+scrollbar = Scrollbar(inputFrame)
+scrollbar.pack(side=RIGHT, fill=Y)
+inputbox.pack()
+scrollbar.config(command=inputbox.yview)
+inputbox.config(yscrollcommand=scrollbar.set)
+
+inputFrame.grid(row=0, column=0)
+
+#creating a browse button
+browseButton = Button(root, text='load', command=browse)
+browseButton.grid(row=1, column=0)
 
 mainloop()
